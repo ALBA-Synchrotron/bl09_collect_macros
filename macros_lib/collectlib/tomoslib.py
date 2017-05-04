6853aa0fcdff4f09fff550c3910cd757d49827da
mrosanes <mrosanes@cells.es>
2017-05-04 17:20:14 +0200

    Modify location of energy in file name
    Modify location of energy in file name in order to fix bug in
    preprocessing and also for better organization of filename.

--- macros_lib/collectlib/tomoslib.py
+++ macros_lib/collectlib/tomoslib.py
@@ -72,8 +72,8 @@ class ManyTomos(GenericTXMcommands):
             theta = self.current_theta
         if energy is None:
             energy = self.current_energy
-        base_name = ('%s_%.1f_%.1f_%.1f' % (sample_name, theta,
-                                            zone_plate, energy))
+        base_name = ('%s_%.1f_%.1f_%.1f' % (sample_name, energy,
+                                            theta, zone_plate))
         extension = 'xrm'
         if self._repetitions == 1 or self._repetitions is None:
             file_name = '%s.%s' % (base_name, extension)
