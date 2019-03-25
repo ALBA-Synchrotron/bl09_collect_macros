cc8be00ab4ccbc55ba2b5c3638258b2be938e92a
mrosanes <mrosanes@cells.es>
2019-03-25 13:07:31 +0100

    Modify wait time from 300 to 180

--- macros_lib/collectlib/tomoslib.py
+++ macros_lib/collectlib/tomoslib.py
@@ -184,7 +184,7 @@ class ManyTomos(GenericTXMcommands):
             self.collect_sample(sample)
             # wait 5 minutes between samples (don't wait for last loop)
             if num < len(self.samples):
-                self.wait(300)
+                self.wait(180)
 
         # Select END action according DS TXMAutoPreprocessing: 4
         self.move_select_action(4)
